27d48502403fcbc3b6cd21afa16c0f8699882558
Kostya Ischuk <slubrusher@yandex.ru>
2024-04-23 14:13:38 +0700

    added create and delete cards functions

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const listContainer = document.querySelector('.places__list'); 
+
+function addCard (cardName, cardLink, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardElement.querySelector('.card__image').src = cardLink;
+  
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+	});
+
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(elem => {
+  const card = addCard(elem.name, elem.link, deleteCard);
+  
+  listContainer.append(card);
+});
